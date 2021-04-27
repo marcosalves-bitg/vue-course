5653f42f83617b470c4f9f4536c8c210cb872bdb
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2021-04-27 16:44:49 -0300

    termino seção 3. Termino desafio 5

---
+++ 4-condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		arrayOfGames: ['Fifa 20', 'Red Dead Redemption', 'Star Wars Jedi Fallen Order'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		valueShow: false,
+	}
+});
