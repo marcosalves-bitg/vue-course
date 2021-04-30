fc558fd0d98bc8e368a7c608c11552f0ca9dd24c
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2021-04-30 16:58:45 -0300

    finalização seção 6

---
+++ 6-vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id='app'>
+    {{ titulo }}
+    <button @click="titulo += '#'">alterar</button>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        titulo: 'Olá Mundo Vue'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: #543764;
+    color: #fff
+  }
+</style>
